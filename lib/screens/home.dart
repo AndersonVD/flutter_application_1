3cabc235ab41beacb2314b3ef74c30693fd4ddaf
Anderson Viana <anderson1565@live.com>
2022-11-22 10:35:58 -0300

    grid was applied also new design
    agora utilizo grid para ordenar as imagens e adicionar um botao tambem

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -26,37 +26,71 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('jhgkjh'),
-      ),
-      body: _userModel == null || _userModel!.isEmpty
-          ? const Center(
-              child: CircularProgressIndicator(
-                  backgroundColor: Color.fromARGB(255, 28, 26, 26)),
-            )
-          : ListView.builder(
-              itemCount: _userModel!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        appBar: AppBar(
+          title: const Text('Ghibli Films'),
+        ),
+        body: _userModel == null || _userModel!.isEmpty
+            ? const Center(
+                child: CircularProgressIndicator(
+                    backgroundColor: Color.fromARGB(255, 28, 26, 26)),
+              )
+            : GridView.count(
+                crossAxisCount: 2,
+                padding: const EdgeInsets.all(1),
+                childAspectRatio: 1.0,
+                mainAxisSpacing: 2.0,
+                crossAxisSpacing: 1.0,
+                scrollDirection: Axis.vertical,
+                children: [
+                  for (var i = 0; i < _userModel!.length; i++)
+                    Card(
+                      child: Column(
                         children: [
-                          // Text(_userModel![index].id.toString()),
-                          Text(_userModel![index].title),
-                          Text(_userModel![index].director),
+                          Image.network(_userModel![i].image,
+                              width: 100, height: 100),
+                          Text(_userModel![i].title),
+                          ButtonBar(
+                            alignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  print(_userModel![i].id);
+                                  Navigator.pushNamed(context, '/tela2',
+                                      arguments: _userModel![i]);
+                                },
+                                child: const Text('Detalhes'),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
-                      const SizedBox(
-                        height: 20.0,
-                      ),
-                      Image.network(_userModel![index].image)
-                    ],
-                  ),
-                );
-              },
-            ),
-    );
+                    ),
+                ],
+              )
+
+        // ListView.builder(
+        //     itemCount: _userModel!.length,
+        //     itemBuilder: (context, index) {
+        //       return Card(
+        //         child: Column(
+        //           children: [
+        //             Row(
+        //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //               children: [
+        //                 // Text(_userModel![index].id.toString()),
+        //                 Text(_userModel![index].title),
+        //                 Text(_userModel![index].director),
+        //               ],
+        //             ),
+        //             const SizedBox(
+        //               height: 20.0,
+        //             ),
+        //             Image.network(_userModel![index].image)
+        //           ],
+        //         ),
+        //       );
+        //     },
+        //   ),
+        );
   }
 }
